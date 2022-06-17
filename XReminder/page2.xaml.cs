c0d710d7f322e16895d8c85bb8009502d727bb34
Ghiath Sardast <Ghiath_Sardast@sluz.ch>
2022-06-17 13:31:26 +0200

    Checked Filter

--- XReminder/page2.xaml.cs
+++ XReminder/page2.xaml.cs
@@ -29,20 +29,29 @@ namespace XReminder
         int aktID = 0;
         int detailID = 0;
         int checkID = 0;
+        string listMode;
         Dictionary<int, Dictionary<string, string>> Elements;
 
         private MainWindow mainWindow = null;
 
-        public page2(MainWindow w, int ID, Dictionary<int, Dictionary<string, string>> allElements)
+        public page2(MainWindow w, int ID, Dictionary<int, Dictionary<string, string>> allElements, string mode = "all")
         {
             InitializeComponent();
             mainWindow = w;
             aktID = ID;
             Elements = allElements;
+            listMode = mode;
         }
 
         private void FillReminders(object sender, RoutedEventArgs e)
         {
+            if(listMode == "checked")
+            {
+                modeBtn.Background = (Brush)this.FindResource("checkAllIcon_checked");
+            } else
+            {
+                modeBtn.Background = (Brush)this.FindResource("checkAllIcon");
+            }
             if (Elements != null)
             {
                 foreach (KeyValuePair<Int32, Dictionary<string, string>> entryRems in Elements)
@@ -50,91 +59,95 @@ namespace XReminder
                     Int32 eleIndex = entryRems.Key;
                     Dictionary<string, string> eleContent = entryRems.Value;
 
-                    remID = eleContent["ID"];
-                    remTitel = eleContent["Titel"];
-                    remDatum = eleContent["Datum"];
-                    remKat = eleContent["Kat"];
-                    remBem = eleContent["Bem"];
-                    remPrio = eleContent["Prio"];
-
-                    Grid remGrid = new Grid();
-                    remGrid.Margin = new Thickness(20, 20, 20, 0);
-                    remGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
-                    remGrid.VerticalAlignment = VerticalAlignment.Top;
-
-                    RowDefinition gridRow1 = new RowDefinition();
-                    gridRow1.Height = new GridLength(40);
-                    RowDefinition gridRow2 = new RowDefinition();
-                    gridRow2.Height = new GridLength(40);
-                    remGrid.RowDefinitions.Add(gridRow1);
-                    remGrid.RowDefinitions.Add(gridRow2);
-
-                    ColumnDefinition gridCol1 = new ColumnDefinition();
-                    gridCol1.Width = new GridLength();
-                    ColumnDefinition gridCol2 = new ColumnDefinition();
-                    gridCol2.Width = new GridLength(50);
-                    ColumnDefinition gridCol3 = new ColumnDefinition();
-                    gridCol3.Width = new GridLength(20);
-                    ColumnDefinition gridCol4 = new ColumnDefinition();
-                    gridCol4.Width = new GridLength(50);
-                    remGrid.ColumnDefinitions.Add(gridCol1);
-                    remGrid.ColumnDefinitions.Add(gridCol2);
-                    remGrid.ColumnDefinitions.Add(gridCol3);
-                    remGrid.ColumnDefinitions.Add(gridCol4);
-
-                    TextBlock txtBlock1 = new TextBlock();
-                    txtBlock1.SetValue(Grid.RowProperty, 0);
-                    txtBlock1.SetValue(Grid.ColumnProperty, 0);
-                    txtBlock1.TextAlignment = TextAlignment.Left;
-                    txtBlock1.FontSize = 30;
-                    txtBlock1.Text = remTitel;
-                    txtBlock1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
-                    remGrid.Children.Add(txtBlock1);
-
-                    TextBlock txtBlock2 = new TextBlock();
-                    txtBlock2.SetValue(Grid.RowProperty, 1);
-                    txtBlock2.SetValue(Grid.ColumnProperty, 0);
-                    txtBlock2.TextAlignment = TextAlignment.Left;
-                    txtBlock2.VerticalAlignment = VerticalAlignment.Bottom;
-                    txtBlock2.FontSize = 15;
-                    txtBlock2.Text = remDatum + " - " + remBem;
-                    txtBlock2.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
-                    remGrid.Children.Add(txtBlock2);
-
-                    Style style = new Style(typeof(Border));
-                    style.Setters.Add(new Setter(Border.CornerRadiusProperty, new CornerRadius(10)));
-
-                    Button btn1 = new Button();
-                    btn1.SetValue(Grid.RowProperty, 0);
-                    btn1.SetValue(Grid.ColumnProperty, 1);
-                    btn1.SetValue(Grid.RowSpanProperty, 2);
-                    btn1.Width = 50;
-                    btn1.Height = 50;
-                    btn1.Resources.Add(typeof(Border), style);
-                    if(eleContent["Erledigt"] == "Nein")
+                    if (listMode == "all" || listMode == "checked" && eleContent["Erledigt"] == "Ja")
                     {
-                        btn1.Background = (Brush)this.FindResource("checkIcon");
-                    } else
-                    {
-                        btn1.Background = (Brush)this.FindResource("greenCheckIcon");
+                        remID = eleContent["ID"];
+                        remTitel = eleContent["Titel"];
+                        remDatum = eleContent["Datum"];
+                        remKat = eleContent["Kat"];
+                        remBem = eleContent["Bem"];
+                        remPrio = eleContent["Prio"];
+
+                        Grid remGrid = new Grid();
+                        remGrid.Margin = new Thickness(20, 20, 20, 0);
+                        remGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
+                        remGrid.VerticalAlignment = VerticalAlignment.Top;
+
+                        RowDefinition gridRow1 = new RowDefinition();
+                        gridRow1.Height = new GridLength(40);
+                        RowDefinition gridRow2 = new RowDefinition();
+                        gridRow2.Height = new GridLength(40);
+                        remGrid.RowDefinitions.Add(gridRow1);
+                        remGrid.RowDefinitions.Add(gridRow2);
+
+                        ColumnDefinition gridCol1 = new ColumnDefinition();
+                        gridCol1.Width = new GridLength();
+                        ColumnDefinition gridCol2 = new ColumnDefinition();
+                        gridCol2.Width = new GridLength(50);
+                        ColumnDefinition gridCol3 = new ColumnDefinition();
+                        gridCol3.Width = new GridLength(20);
+                        ColumnDefinition gridCol4 = new ColumnDefinition();
+                        gridCol4.Width = new GridLength(50);
+                        remGrid.ColumnDefinitions.Add(gridCol1);
+                        remGrid.ColumnDefinitions.Add(gridCol2);
+                        remGrid.ColumnDefinitions.Add(gridCol3);
+                        remGrid.ColumnDefinitions.Add(gridCol4);
+
+                        TextBlock txtBlock1 = new TextBlock();
+                        txtBlock1.SetValue(Grid.RowProperty, 0);
+                        txtBlock1.SetValue(Grid.ColumnProperty, 0);
+                        txtBlock1.TextAlignment = TextAlignment.Left;
+                        txtBlock1.FontSize = 30;
+                        txtBlock1.Text = remTitel;
+                        txtBlock1.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
+                        remGrid.Children.Add(txtBlock1);
+
+                        TextBlock txtBlock2 = new TextBlock();
+                        txtBlock2.SetValue(Grid.RowProperty, 1);
+                        txtBlock2.SetValue(Grid.ColumnProperty, 0);
+                        txtBlock2.TextAlignment = TextAlignment.Left;
+                        txtBlock2.VerticalAlignment = VerticalAlignment.Bottom;
+                        txtBlock2.FontSize = 15;
+                        txtBlock2.Text = remDatum + " - " + remBem;
+                        txtBlock2.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffffff"));
+                        remGrid.Children.Add(txtBlock2);
+
+                        Style style = new Style(typeof(Border));
+                        style.Setters.Add(new Setter(Border.CornerRadiusProperty, new CornerRadius(10)));
+
+                        Button btn1 = new Button();
+                        btn1.SetValue(Grid.RowProperty, 0);
+                        btn1.SetValue(Grid.ColumnProperty, 1);
+                        btn1.SetValue(Grid.RowSpanProperty, 2);
+                        btn1.Width = 50;
+                        btn1.Height = 50;
+                        btn1.Resources.Add(typeof(Border), style);
+                        if (eleContent["Erledigt"] == "Nein")
+                        {
+                            btn1.Background = (Brush)this.FindResource("checkIcon");
+                        }
+                        else
+                        {
+                            btn1.Background = (Brush)this.FindResource("greenCheckIcon");
+                        }
+                        btn1.Tag = remID.ToString();
+                        btn1.Click += new RoutedEventHandler(checkReminder);
+                        remGrid.Children.Add(btn1);
+
+                        Button btn2 = new Button();
+                        btn2.SetValue(Grid.RowProperty, 0);
+                        btn2.SetValue(Grid.ColumnProperty, 3);
+                        btn2.SetValue(Grid.RowSpanProperty, 2);
+                        btn2.Width = 50;
+                        btn2.Height = 50;
+                        btn2.Resources.Add(typeof(Border), style);
+                        btn2.Background = (Brush)this.FindResource("suchIcon");
+                        btn2.Tag = remID.ToString();
+                        btn2.Click += new RoutedEventHandler(DetailButton_Click);
+                        remGrid.Children.Add(btn2);
+
+                        Reminders.Children.Add(remGrid);
                     }
-                    btn1.Tag = remID.ToString();
-                    btn1.Click += new RoutedEventHandler(checkReminder);
-                    remGrid.Children.Add(btn1);
-
-                    Button btn2 = new Button();
-                    btn2.SetValue(Grid.RowProperty, 0);
-                    btn2.SetValue(Grid.ColumnProperty, 3);
-                    btn2.SetValue(Grid.RowSpanProperty, 2);
-                    btn2.Width = 50;
-                    btn2.Height = 50;
-                    btn2.Resources.Add(typeof(Border), style);
-                    btn2.Background = (Brush)this.FindResource("suchIcon");
-                    btn2.Tag = remID.ToString();
-                    btn2.Click += new RoutedEventHandler(DetailButton_Click);
-                    remGrid.Children.Add(btn2);
-
-                    Reminders.Children.Add(remGrid);
                 }
             }
         }
@@ -166,6 +179,19 @@ namespace XReminder
                 Elements[checkID]["Erledigt"] = "Nein";
                 ((Button)sender).Background = (Brush)this.FindResource("checkIcon");
             }
+        }
+
+        private void changeMode(object sender, RoutedEventArgs e)
+        {
+            if(listMode == "all")
+            {
+                listMode = "checked";
+            } else
+            {
+                listMode = "all";
+            }
+            page2 Page2 = new page2(mainWindow, aktID, Elements, listMode);
+            mainWindow.Content = Page2;
         }
     }
 }
